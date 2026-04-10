ec32b04ca205ef0ad430e253b99bd0041d941ecc
KacperZa <lacper.zajac765@gmail.com>
2026-04-10 18:15:38 +0200

    Added one universal function

--- server/server.js
+++ server/server.js
@@ -2,9 +2,31 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 
-app.get('api', async (req, res) => {
-    const answer = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`)
-    const data = await answer.json()
+const TMDB_BASE_URL = 'https://api.themoviedb.org/3';
+
+const tmdbFetch = async (endpoint, params) => {
+    const url = new URL(`${TMDB_BASE_URL}${endpoint}`);
+
+    url.searchParams.set('language', 'en-US')
+
+    Object.entries(params).forEach(([key, value]) => {
+        url.searchParams.set(key, value)
+    })
+
+    const res = await fetch(url.toString(), {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            Authorization: `Bearer ${process.env.TMDB_TOKEN}`
+        },
+    });
+
+    return res.json();
+};
+
+app.get('/api', async (req, res) => {
+    const { page = '1'} = req.query;
+    const data = await tmdbFetch('/movie/popular', { page })
     res.json(data.results)
 })
 
