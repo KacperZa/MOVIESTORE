a54fa72f91e315d9fe88320fb99217c4787e3bcf
KacperZa <lacper.zajac765@gmail.com>
2026-04-10 18:18:18 +0200

    added pagination

--- moviestore/src/PagesComponents/BrowsePage.tsx
+++ moviestore/src/PagesComponents/BrowsePage.tsx
@@ -1,38 +1,60 @@
 import { useEffect, useState } from "react"
-import { Link } from "react-router-dom"
-
+import { Link, useSearchParams } from "react-router-dom"
+import { LesserThanIcon, GreaterThanIcon } from "../components/Icons";
+// import SkeletonImage from "../components/SkeletonImage";
+import 'react-loading-skeleton/dist/skeleton.css'
 function BrowsePage() {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const [backendData, setBackendData] = useState<any[]>([])
+  const [searchParams, setSearchParams] = useSearchParams();
+  const page = Number(searchParams.get('page') || 1);
+  const [isloading, setIsLoading] = useState(true)
+
+  // function for changing pages
+  const changePage = (newPage : number) => {
+    setSearchParams({ page: String(newPage)});
+  }
+  const itemsPerPage = 8;
+
+  // fetching data from backend
   useEffect(() => {
     fetch("/api").then(
       response => response.json()
     ).then(
       data =>{
-        console.log(data)
         setBackendData(data)
+        setIsLoading(false)
       }
     )
   },[])
 
-  return (
-    <div className="grid grid-cols-4 gap-y-7 p-10 ml-5 rounded-2xl bg-green-600">
+  // pagination 
+  const start = (page - 1) * itemsPerPage
+  const currentMovies = backendData.slice(start, start + itemsPerPage)
 
-      {/* 1523145 */}
-      {(typeof backendData === 'undefined') ? (
-        <p> Loading...</p>
+  return (
+  <>
+  <div className="flex flex-row ml-5 rounded-2xl bg-green-400 justify-center items-center p-2">
+    <p onClick={() => page > 1 ? changePage(page - 1) : null } className="cursor-pointer"><LesserThanIcon color="black" size={60}/></p>
+    <div className="grid grid-cols-4 gap-y-7 p-10  justify-center items-center">
+      {(isloading) ? (
+        // <SkeletonImage cards={8}/>
+        <p>Loading...</p>
       ):
       (
-        backendData.map((user, id) => (
-          <div className="bg">
+        currentMovies.map((user, id) => (
+          <div key={id}>
             <Link to={`/film/${user.id}`} key={id}>
-              <img src={`https://image.tmdb.org/t/p/w500/${user.poster_path}`} alt="" className="h-auto w-8/10 rounded-xl shadow-lg/20" />
+              <img src={`https://image.tmdb.org/t/p/w500/${user.poster_path}`} alt="" className="h-auto w-7/10 rounded-xl shadow-lg/20 justify-self-center select-none" />
             </Link>
           </div>
         ))
       )
-    }
+      }
     </div>
+    <p onClick={() => page < 3 ? changePage(page + 1) : null } className="cursor-pointer"><GreaterThanIcon color="black" size={60}/></p>
+  </div>
+</>
   )
 }
 
