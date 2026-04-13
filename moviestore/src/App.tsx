664cacecea2170dc2966553a37d17a25e5eb0ac4
KacperZa <lacper.zajac765@gmail.com>
2026-04-14 00:33:49 +0200

    added context with movie genres

--- moviestore/src/App.tsx
+++ moviestore/src/App.tsx
@@ -3,16 +3,33 @@ import './App.css'
 import Menu from './components/Menu'
 import HomePage from './PagesComponents/HomePage'
 import BrowsePage from './PagesComponents/BrowsePage'
+import { useEffect, useState } from 'react'
+import { GenreContext } from './img/context/GenreContext'
+
 
 function App() {
+  const [genre, setGenre] = useState(null);
+  // const [loading, setLoading] = useState(true);
+
+
+  useEffect(() =>{
+    fetch("/api/movie/genres")
+    .then(r => r.json())
+    .then(data => {
+      setGenre(data)
+      // setLoading(false)
+    })
+  },[])
 
   return (
   <>
     <Menu>
-      <Routes>
-        <Route path="/" element={<HomePage/>}></Route>
-        <Route path="/browse" element={<BrowsePage/>}></Route>
-      </Routes>
+      <GenreContext.Provider value={genre}>
+        <Routes>
+          <Route path="/" element={<HomePage/>}></Route>
+          <Route path="/browse" element={<BrowsePage/>}></Route>
+        </Routes>
+      </GenreContext.Provider>
     </Menu>
   </>
 )
