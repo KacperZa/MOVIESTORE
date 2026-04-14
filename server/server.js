03527be24b71fb0a951c4298219092622fdd3a29
KacperZa <lacper.zajac765@gmail.com>
2026-04-14 18:20:45 +0200

    added more routes

--- server/server.js
+++ server/server.js
@@ -28,17 +28,33 @@ app.get('/api', async (req, res) => {
     const data = await tmdbFetch('/movie/popular', { page })
     res.json(data.results)
 })
+// MOVIE GENRES
+app.get('/api/movie/genres', async (req, res) => {
+    const data = await tmdbFetch('/genre/movie/list')
+    res.json(data.genres)
+})
+
+// MOVIE WITH SPEFICIC GENRE
+app.get('/api/movie/:genre_id', async (req, res) => {
+    const genreId = req.params.genre_id;
+    const data = await tmdbFetch(`/discover/movie?with_genres=${genreId}`)
+    res.json(data.results)
+})
+
+// MOVIE DETAILS
+app.get('/api/movie/details/:movieId', async (req, res) => {
+    const movieId = req.params['movieId']
+    const data = await tmdbFetch(`/movie/${movieId}`)
+    res.json(data.genres)
+})
+
+// TV SHOWS
 app.get('/api/tv', async (req, res) => {
     const { page = '1'} = req.query;
     const data = await tmdbFetch('/tv/changes', { page })
     res.json(data.results)
 })
 
-// MOVIE GENRES
-app.get('/api/movie/genres', async (req, res) => {
-    const data = await tmdbFetch('/genre/movie/list')
-    res.json(data.genres)
-})
 
 // TV SHOWS GENRES
 app.get('/api/tv/genres', async (req, res) => {
