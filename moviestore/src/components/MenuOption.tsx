f3da4e4a67d6f7729c56ce707c4bc268d6b876d8
KacperZa <lacper.zajac765@gmail.com>
2026-04-14 18:19:29 +0200

    Added animation and changed width

--- moviestore/src/components/MenuOption.tsx
+++ moviestore/src/components/MenuOption.tsx
@@ -1,28 +1,32 @@
 import type React from "react"
-import { motion } from "motion/react"
+import { motion, spring } from "motion/react"
 import { Link } from "react-router-dom"
-
 interface MenuOptionsProps {
-    text: string
-    icon: React.ReactNode
-    url: string
+  text: string
+  icon: React.ReactNode
+  url: string
 }
 
-function MenuOption({text, icon, url}: MenuOptionsProps) {
+function MenuOption({ text, icon, url}: MenuOptionsProps) {
+
+
+
   return (
     <>
-    <Link to={url}>
-      <motion.div 
-      className="flex flex-row gap-3 bg-gray-950 w-fit p-1.5 p-px-2 rounded-xl select-none"
-      initial= {{ x: 0}}
-      whileHover={{ x:10}}
-      >
-          {icon}
-          <div className='text-2xl font-medium text-gray-200'>
-            {text}
-            </div>
-      </motion.div>
-    </Link>
+    <div className="relative group w-fit">
+        <Link to={url} className=" w-fit">
+            <motion.div 
+            className="flex flex-row gap-3 bg-gray-950 w-fit p-1.5 p-px-2 rounded-xl select-none"
+            initial= {{ x: 0}}
+            whileHover={{ x:10}}
+            transition={{type: spring, stiffness: 150, damping: 8, mass: 1 }}>
+                {icon}
+                <div className='text-2xl font-medium text-gray-200'>
+                  {text}
+                  </div>
+            </motion.div>
+        </Link>
+      </div>
     </>
   )
 }
