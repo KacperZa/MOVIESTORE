274aa74d0186fc8eaac5c5d96e9b518e5997455c
KacperZa <lacper.zajac765@gmail.com>
2026-04-14 18:18:45 +0200

    minimal change in menu's look

--- moviestore/src/components/Menu.tsx
+++ moviestore/src/components/Menu.tsx
@@ -2,6 +2,7 @@ import MenuOption from './MenuOption'
 // import { motion } from 'motion/react'
 import {HeartIcon, MagnifyingGlassIcon, SkipIcon, SettingsIcon, FavouriteIcon, HomeIcon} from './Icons'
 import { motion } from 'motion/react'
+import MenuOptionBrowse from './MenuOptionBrowse'
 
 interface MenuProps {
   children: React.ReactNode
@@ -12,19 +13,19 @@ function Menu({children}: MenuProps) {
   
   return (
   <>
-    <motion.div className="flex flex-row font-inter p-6 bg-gray-200 w-full  mb-0 ">  {/* OPAKOWANIE CALEJ STRONY */}
-    <div className="flex flex-col">
+    <motion.div className="flex flex-row font-inter p-6 bg-gray-200 w-full max-h-screen mb-0 overflow-hidden">  {/* OPAKOWANIE CALEJ STRONY */}
+    <div className="flex flex-col max-h-screen justify-between">
       {/* SIDEBAR */}
-      <motion.div className="flex flex-col gap-14 h-100% bg-blue-400 p-6 rounded-2xl sticky top-6  w-1/1 "
+      <motion.div className="flex flex-col gap-13 max-h-screen bg-blue-400 p-6 py-11 rounded-2xl w-90 top-6 overflow-y-auto"
       // initial = {{opacity:1, x: 0}}
       // whileHover={{opacity: 0, x:-400}} 
       > 
         <div className="text-5xl font-istok-web font-semibold">MOVIESTORE</div>
-        <div className='flex flex-col gap-8'>
+        <div className='flex flex-col gap-8 max-h-full'>
             <div className="text-4xl font-bold">Menu</div>
             <div className="flex flex-col gap-9">
               <MenuOption text={"Home"} url='/' icon={<HomeIcon/>}/>
-              <MenuOption text={"Browse"} url='/browse' icon={<MagnifyingGlassIcon/>}/>
+              <MenuOptionBrowse text={"Browse"} url='/browse' icon={<MagnifyingGlassIcon/>}/>
               <MenuOption text={"Coming soon"} url='/browse' icon={<SkipIcon/>}/>
               <MenuOption text={"Watchlist"} url='/browse' icon={<HeartIcon/>}/>
               {/* TODO ZMIENIC IKONE  */}
@@ -33,7 +34,7 @@ function Menu({children}: MenuProps) {
             </div>
           </div>
         <div className='flex flex-col gap-8'>
-        <div className="text-4xl font-bold">General</div>
+        <div className="text-4xl font-bold ">General</div>
         <div className="flex flex-col gap-9">
               <MenuOption text={"Settings"} url='/browse' icon={<SettingsIcon/>}/>
               <MenuOption text={"Settings"} url='/browse' icon={<SettingsIcon/>}/>
