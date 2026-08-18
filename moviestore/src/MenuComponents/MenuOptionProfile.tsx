540a1cf76170b19b78dfb8eb953c0a028ff0a164
KacperZa <lacper.zajac765@gmail.com>
2026-08-18 17:09:32 +0200

    feat: enhance MenuOptionProfile with click outside functionality and theme switch

--- moviestore/src/MenuComponents/MenuOptionProfile.tsx
+++ moviestore/src/MenuComponents/MenuOptionProfile.tsx
@@ -1,8 +1,10 @@
 import { AnimatePresence, motion } from "motion/react"
 import {  Link, useNavigate } from "react-router-dom"
 import { Settings } from "lucide-react"
-import { useState } from "react"
+import { useRef, useState } from "react"
 import { useUser } from "@/context/useUser"
+import { useClickOutside } from "@/hooks/useClickOutside"
+import ThemeSwitch from "@/ui/ThemeSwitch"
 
 
 
@@ -10,31 +12,34 @@ function MenuOptionProfile() {
   const [isVisible, setIsVisible ] = useState(false)
   const navigate = useNavigate()
 
-
-
-  const handleClick = () => {
+  
+  const handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
     if(user !== null) {
       setIsVisible(prev => !prev)
+      e.stopPropagation()
     } else {
       navigate('/login')
     }
   }
 
-  const {user} = useUser()
+  const {user} = useUser()  
+  const ref = useRef<HTMLDivElement>(null)
+
+  useClickOutside({ref, callback: () => setIsVisible(false)})
 
   return (
     <>
-    <div className="relative w-full">
-        <button className="w-full cursor-pointer" onClick={handleClick}>
+    <div className="relative w-full" ref={ref}>
+        <div className="w-full cursor-pointer" onClick={handleClick} >
             <motion.div 
-            className="flex flex-row items-center gap-3 p-2 rounded-lg hover:bg-blue-500 transition-all ease-in-out duration-350"
+            className="group flex flex-row items-center gap-3 p-2 rounded-lg hover:text-accent transition-all ease-in-out duration-350"
             layout>
-                <Settings size={30} />
+                <Settings size={30} className="group-hover:rotate-180" />
                 <div className='text-lg font-medium'>
                   Profile
                   </div>
             </motion.div>
-        </button>
+        </div>
         <AnimatePresence>
           {isVisible &&
             <motion.div className="absolute overflow-hidden shadow-2xl/30 z-30 w-48"
@@ -44,11 +49,13 @@ function MenuOptionProfile() {
             transition={{
               duration: 0.2
             }}>
-              <div className="flex flex-col gap-2 px-3 py-1.5 mt-2 rounded-lg w-full bg-blue-300">
-                <Link to={'/profile'} className="px-3 py-1.5">Profile</Link>
-                <hr />
-                <Link to={'/watch-history'} className="px-3 py-1.5">Watched Films</Link >
-                <Link to={'/browse/tv'} className="px-3 py-1.5">Favourites</Link >
+              <div className="flex flex-col gap-2 px-3 py-1.5 mt-2 rounded-lg w-full bg-background">
+                <Link to={'/profile'} className="px-3 py-1.5" onClick={() => setIsVisible(false)}>Profile</Link>
+                <hr  className="border-b border-card"/>
+                <Link to={'/watch-history'} className="px-3 py-1.5" onClick={() => setIsVisible(false)}>Watched Films</Link >
+                <Link to={'/favourites'} className="px-3 py-1.5" onClick={() => setIsVisible(false)}>Favourites</Link >
+                <ThemeSwitch />
+                  
               </div>
             </motion.div>
           }
@@ -59,3 +66,5 @@ function MenuOptionProfile() {
 }
 
 export default MenuOptionProfile
+
+
