86f8cf815f6d4055f42bedd1c8116c5978c4234c
KacperZa <lacper.zajac765@gmail.com>
2026-08-18 17:22:25 +0200

    style: removed layout prop

--- moviestore/src/ui/Switch.tsx
+++ moviestore/src/ui/Switch.tsx
@@ -18,7 +18,7 @@ function Switch({value, setValue, sliderColor = "bg-gray-400", sliderColorChange
     
   return (
     <>
-        <motion.button layout className={`flex ${value ? `justify-end ${backgroundColorChange}` : `justify-start ${backgroundColor}`} w-12 h-6 cursor-pointer p-1 px-2 rounded-xl font-medium`} onClick={handleSwitch}>
+        <motion.button className={`flex ${value ? `justify-end ${backgroundColorChange}` : `justify-start ${backgroundColor}`} w-12 h-6 cursor-pointer p-1 px-2 rounded-xl font-medium`} onClick={handleSwitch}>
             <motion.div layout className={`h-full w-1/2 rounded-2xl  ${value ? `${sliderColorChange}`: `${sliderColor}`}`}></motion.div>
         </motion.button>
     </>
