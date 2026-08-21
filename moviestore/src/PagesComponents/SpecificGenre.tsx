a61ee7fbe00e008a59b86fc663526694e4d1e369
KacperZa <lacper.zajac765@gmail.com>
2026-08-21 17:41:11 +0200

    refactor: Remove addHistory prop from MediaCard component(SpecificGenre.tsx)

--- moviestore/src/PagesComponents/SpecificGenre.tsx
+++ moviestore/src/PagesComponents/SpecificGenre.tsx
@@ -76,9 +76,9 @@ function SpecificGenre() {
             (
               films.map((film, i) => {
                 if(films.length === i + 1) {
-                  return <MediaCard<FilmsWithGenres> key={i} lastMediaElementRef={lastMediaElementRef}  media={film} type={type} favouriteIds={favouriteIds} setFavouriteIds={setFavouriteIds} addFavourite={addFavourite} removeFavourite={removeFavourite} addToHistory showGenres isRef/>
+                  return <MediaCard<FilmsWithGenres> key={i} lastMediaElementRef={lastMediaElementRef}  media={film} type={type} favouriteIds={favouriteIds} setFavouriteIds={setFavouriteIds} addFavourite={addFavourite} removeFavourite={removeFavourite} showGenres isRef/>
                 } else {
-                  return <MediaCard<FilmsWithGenres> key={i} lastMediaElementRef={lastMediaElementRef}  media={film} type={type} favouriteIds={favouriteIds} setFavouriteIds={setFavouriteIds} addFavourite={addFavourite} removeFavourite={removeFavourite} addToHistory showGenres/>
+                  return <MediaCard<FilmsWithGenres> key={i} lastMediaElementRef={lastMediaElementRef}  media={film} type={type} favouriteIds={favouriteIds} setFavouriteIds={setFavouriteIds} addFavourite={addFavourite} removeFavourite={removeFavourite} showGenres/>
                 } })
             )
             }
