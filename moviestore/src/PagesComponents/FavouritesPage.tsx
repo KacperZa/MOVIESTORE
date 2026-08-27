de879c9dce30847e65d5f62a5c5b12a308b7ae57
KacperZa <lacper.zajac765@gmail.com>
2026-08-27 11:45:41 +0200

    feat: Enhance Favourites and WatchedFilms components with empty state handling and navigation buttons

--- moviestore/src/PagesComponents/FavouritesPage.tsx
+++ moviestore/src/PagesComponents/FavouritesPage.tsx
@@ -3,6 +3,8 @@ import MediaCard, { type FilmsWithGenres } from '@/ui/MediaCard'
 import FavouriteToggle from './FavouriteToggle'
 import useFetchFavouritesIds from '@/hooks/useFetchFavouritesIds'
 import { useUser } from '@/context/useUser'
+import { motion } from 'motion/react'
+import { useNavigate } from 'react-router-dom'
 
 export interface MediaWithUser extends FilmsWithGenres {
   userId: string
@@ -33,20 +35,51 @@ function Favourites() {
 
   const { addFavourite, removeFavourite} = FavouriteToggle()
 
-    const { favouriteIds, setFavouriteIds } = useFetchFavouritesIds(user?._id)
+  const { favouriteIds, setFavouriteIds } = useFetchFavouritesIds(user?._id)
 
-
+  const navigate = useNavigate()
+  
     return (
     <>
-        <div className='flex flex-col w-full h-full border-t border-card p-2 items-center scrollbar-thumb-primary scrollbar-gutter-stable'>
+        <div className='flex flex-col w-full h-full border-t border-card p-2 items-center scrollbar-thumb-primary scrollbar-gutter-stable bg-radial from-card from-5% to-background'>
             <div className='text-3xl p-5 font-bold tracking-wide text-secondary'>Favourites</div>
-            <div className='grid grid-cols-4 gap-y-5 overflow-auto w-full h-full justify-center'>
-                {favourites?.map(media =>{
-                  return <MediaCard<DetailsWithUser> key={media.id} media={media} type={media.mediaType} favouriteIds={favouriteIds} setFavouriteIds={setFavouriteIds} addFavourite={addFavourite} removeFavourite={removeFavourite}/>
+                {favourites?.length !== 0 ?
+                    <div className='grid grid-cols-4 gap-y-5 overflow-auto w-full h-full justify-center'>
+                  {favourites?.map(media =>
+                  {
+                    return <MediaCard<DetailsWithUser> key={media.id} media={media} type={media.mediaType} favouriteIds={favouriteIds} setFavouriteIds={setFavouriteIds} addFavourite={addFavourite} removeFavourite={removeFavourite}/>
+                    }
+                  )}
+                    </div>
+                  :
+                  <>
+                  <div className='h-full w-full flex justify-center items-center flex-col gap-2'>
+                    <p className='text-text py-2 px-4 font-medium text-lg'>No favourite media found.</p>
+                    <div className='flex flex-row gap-3'>
+                      <motion.button 
+                      className='bg-secondary px-4 py-2 border-4 border-accent rounded-lg shadow-2xl/50 shadow-secondary cursor-pointer'
+                      onClick={() => navigate('/browse/movie')}
+                      initial={{y: 0}}
+                      whileHover={{y: 1}}
+                      whileTap={{scale: 0.95}}
+                      >
+                        View movies
+                      </motion.button>
+
+                      <motion.button 
+                      className='bg-secondary px-4 py-2 border-4 border-accent rounded-lg shadow-2xl/50 shadow-secondary cursor-pointer'
+                      onClick={() => navigate('/browse/tv')}
+                      initial={{y: 0}}
+                      whileHover={{y: 1}}
+                      whileTap={{scale: 0.95}}
+                      >
+                        View TV shows
+                      </motion.button>
+
+                    </div>
+                  </div>
+                  </>
                 }
-                )}
-    
-            </div>
         </div>
     </>
   )
--- moviestore/src/PagesComponents/WatchedFilms.tsx
+++ moviestore/src/PagesComponents/WatchedFilms.tsx
@@ -5,6 +5,8 @@ import useFetchFavouritesIds from '../hooks/useFetchFavouritesIds'
 import FavouriteToggle from './FavouriteToggle'
 import useFetchWatchedMedia from '../hooks/useFetchWatchedMedia'
 import MediaCard from '@/ui/MediaCard'
+import { motion } from 'motion/react'
+import { useNavigate } from 'react-router-dom'
 
 
 function WatchedFilms() {
@@ -14,16 +16,51 @@ function WatchedFilms() {
   const { favouriteIds, setFavouriteIds } = useFetchFavouritesIds(user?._id)
   const { addFavourite, removeFavourite } = FavouriteToggle()
   const { watchedFilms } = useFetchWatchedMedia()
+
+  const navigate = useNavigate()
     
     return (
       <>
-        <div className='flex flex-col w-full h-full border-t border-card p-2 items-center'>
-            <div className='text-3xl p-5 font-bold tracking-wide text-secondary'>Watched films</div>
+        <div className='flex flex-col w-full h-full border-t border-card p-2 items-center scrollbar-thumb-primary scrollbar-gutter-stable bg-radial from-card from-5% to-background'>
+            <p className='text-3xl p-5 font-bold tracking-wide text-secondary text-shadow-2xl shadow-secondary'>Watched films</p>
+              {watchedFilms.length !== 0 ?
             <div className='grid grid-cols-4 gap-y-5 overflow-auto w-full'>
-              {[...watchedFilms].map((media, i) => {
-                  return <MediaCard<MediaWithUser> key={i}  media={media} type={media.type} favouriteIds={favouriteIds} setFavouriteIds={setFavouriteIds} addFavourite={addFavourite} removeFavourite={removeFavourite} addToHistory/>
+              {watchedFilms.map(media => {
+                  return <MediaCard<MediaWithUser> key={media.id}  media={media} type={media.type} favouriteIds={favouriteIds} setFavouriteIds={setFavouriteIds} addFavourite={addFavourite} removeFavourite={removeFavourite}/>
                 })}
             </div>
+
+                :
+                <>
+                  <div className='h-full w-full flex justify-center items-center flex-col gap-2'>
+                    <p className='text-text py-2 px-4 font-medium text-lg'>Your watch history is empty.</p>
+
+                    <div className='flex flex-row gap-3'>
+                      <motion.button 
+                      className='bg-secondary px-4 py-2 border-4 border-accent rounded-lg shadow-2xl shadow-secondary cursor-pointer'
+                      onClick={() => navigate('/browse/movie')}
+                      initial={{y: 0}}
+                      whileHover={{y: 1}}
+                      whileTap={{scale: 0.95}}
+                      >
+                        View movies
+                      </motion.button>
+
+                      <motion.button 
+                      className='bg-secondary px-4 py-2 border-4 border-accent rounded-lg shadow-2xl shadow-secondary cursor-pointer'
+                      onClick={() => navigate('/browse/tv')}
+                      initial={{y: 0}}
+                      whileHover={{y: 1}}
+                      whileTap={{scale: 0.95}}
+                      >
+                        View TV shows
+                      </motion.button>
+                    </div>
+
+                  </div>
+                </>
+
+              }
         </div>
     </>
   )
