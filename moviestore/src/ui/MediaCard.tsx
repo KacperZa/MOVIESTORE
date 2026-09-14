ab2659cb563a3ae4922c1937a1b25931db1bd421
KacperZa <lacper.zajac765@gmail.com>
2026-09-14 18:59:53 +0200

    refactor: Remove unnecessary props

--- moviestore/src/ui/MediaCard.tsx
+++ moviestore/src/ui/MediaCard.tsx
@@ -14,13 +14,9 @@ export interface Films {
     id: number
     original_language?: string
     original_title?: string
-    overview: string
-    popularity: number
     poster_path: string
-    release_date: string
     title?: string
     name?: string
-    video: boolean
     vote_average: number
     vote_count: number
 }
